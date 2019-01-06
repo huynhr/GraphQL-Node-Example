78a9b71155ab1a28808707bc11f298437789fbcc
Raymond Huynh <raymond.h@myplanet.com>
2019-01-06 17:45:48 -0500

    Add filter, pagination and sorting

---
+++ src/resolvers/Subscription.js
@@ -0,0 +1,29 @@
+// new links
+function newLinkSubscribe(parent, args, context, info) {
+  return context.prisma.$subscribe.link({ mutation_in: ['CREATED'] }).node()
+}
+
+const newLink = {
+  subscribe: newLinkSubscribe,
+  resolve: payload => {
+    return payload
+  },
+}
+
+// New votes
+function newVoteSubscribe(parent, args, context, info) {
+  return context.prisma.$subscribe.vote({ mutation_in: ['CREATED'] }).node()
+}
+
+const newVote = {
+  subscribe: newVoteSubscribe,
+  resolve: payload => {
+    return payload
+  },
+}
+
+// exports
+module.exports = {
+  newLink,
+  newVote,
+}
